b61d3426e2246b29bf93a3adea8175b49aade7ab
LuisDLF98 <ldelafuente34@gmail.com>
2021-09-02 11:59:58 -0500

    Added User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    validates :name, 
+        presence: true, 
+        length: { in: 2..20, too_long: "Username should be at most 20 characters long.", too_short: "Username should be at least 2 characters long." },
+        uniqueness: { message: "Username is already taken" },
+        format: { with: /\A^([a-zA-Z\d]+)$\z/, message: "Username cannot contain whitespace or special characters." }
+
+    validates :password, 
+        presence: true, 
+        length: { in: 6..20, too_long: "Password should be at most 20 characters long.", too_short: "Password should be at least 6 characters long." },
+        uniqueness: { message: "Password is already taken." },
+        format: { with: /\A^([^\s]+)$\z/, message: "Password cannot contain whitespace." },
+        confirmation: true
+end
